f8d607b28eefb0d1f201b33e95a1d576529d7a71
Ggorets0dev <nikgorets4work@gmail.com>
2025-09-05 16:16:52 +0300

    chore(cmake): Рефакторинг путей

---
+++ inc/IPC/pc_chain.hpp
@@ -0,0 +1,12 @@
+#ifndef IPC_CHAIN_H
+#define IPC_CHAIN_H
+
+// >>> Special header for IPC information. Can be included in parent/child processes
+
+#define RGC_INSTALL_PATH                "/home/ggorets0/.local/bin/RuGeolistsCreator"
+
+#define RGC_RELEASE_NOTES_FIFO_PATH     "/tmp/ru-geolists-creator/rgc.fifo"
+
+#define RGC_RELEASE_NOTES_FIFO_PERMS    0666
+
+#endif // IPC_CHAIN_H
